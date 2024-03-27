ab9c834c404a429088a6d3196bdc0a48b47e4833
Albar Pambagio Arioseto <albarpambagio@gmail.com>
2024-03-27 09:24:09 +0700

    fix bugs host value (dev n prod mode)

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -13,9 +13,9 @@ const init = async () => {
   });
 
   server.route(routes);
- 
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
